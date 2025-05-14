b2378c062a99ff22f9c4fc7afca8c38b7ee6b40a
staheys <andrew.dron13@gmail.com>
2025-05-15 01:24:42 +0300

    main files

--- src/smart_detector/__init__.py
+++
@@ -1,8 +0,0 @@
-# src/smart_detector/__init__.py
-from importlib.metadata import version
-
-__version__ = version("smart-detector") # If your package is installed
-
-# You can re-export key functions or variables if desired
-# from .core import some_function
-# from .utils import get_config
